b369e1a8fd9e35a79ab7bec959839727952968c9
Volchishe <nestor_machno1@mail.ru>
2020-04-22 18:26:48 +0300

    Level1 + platformmechanic

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private bool ballIsActive;
+    private Vector3 ballPosition;
+    private Vector2 ballInitialForce;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+      // создаем силу, которая будет действовать на шарик
+	  ballInitialForce = new Vector2 (100.0f,300.0f);
+
+	  // переводим шарик в неактивное состояние
+	  ballIsActive = false;
+
+	  // запоминаем положение
+	  ballPosition = transform.position;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetButtonDown ("Jump") == true) {
+        if (!ballIsActive){
+		  // применим силу 
+		  //AddForce(ballInitialForce);
+		  // зададим активное состояние
+		  ballIsActive = !ballIsActive;
+		}
+         
+      }  
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float playerVelocity;
+	public float boundary;
+    private Vector3 playerPosition;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+      // получаем начальную позицию платформы
+	  playerPosition = gameObject.transform.position;
+	  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // делаем горизонтальное движение
+      playerPosition.x += Input.GetAxis ("Horizontal") * playerVelocity;
+ 
+      // отрабатываем выход из игры
+      if (Input.GetKeyDown(KeyCode.Escape)){
+        Application.Quit();
+      }
+ 
+      // обновим позицию платформы
+      transform.position = playerPosition;  
+	  
+	  // проверяем на выход за границы
+      if (playerPosition.x < -boundary) {
+        transform.position = new Vector3 (-boundary, playerPosition.y, playerPosition.z);
+      } 
+      if (playerPosition.x > boundary) {
+        transform.position = new Vector3(boundary, playerPosition.y, playerPosition.z);     
+      }
+
+    }
+}
